14986edfa5e285c53f80e02f9fd9727968f9041d
Elnara Asadova <asadova.eln@gmail.com>
2021-04-24 16:02:49 +0300

    BaseTest and LoginTest updates

--- src/main/java/com/example/selenium/steps/UserSteps.java
+++ src/main/java/com/example/selenium/steps/UserSteps.java
@@ -1,23 +1,32 @@
 package com.example.selenium.steps;
+import static org.testng.Assert.assertEquals;
 
 import org.openqa.selenium.WebDriver;
+import com.example.selenium.pages.LoginPage;
+import com.example.selenium.pages.MainPage;
 
 public class UserSteps {
     private WebDriver driver;
+    private LoginPage loginPage;
+    private MainPage mainPage;
 
     public UserSteps(WebDriver driver) {
         this.driver = driver;
     }
 
     public com.example.selenium.steps.UserSteps login(String username, String password) {
-        // TODO initialize LoginPage, call setUsername, setPassword, and click login methods
+        loginPage = new LoginPage(driver);
+        mainPage = new MainPage(driver);
 
-        //  TODO assert title
+        loginPage.setUsername(username).setPassword(password).clickLogin();
+
+        assertEquals(mainPage.getTitle(), "Secure Area", "Login Failed");
+
         return this;
     }
 
     public UserSteps logout() {
-        // TODO call logout method
+        mainPage.clickLogout();
         return this;
     }
 }
